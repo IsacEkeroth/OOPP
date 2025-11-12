15abebe22ba1ce40cb422e04d657d0e6da29758a
JoosefL <joseflot@gmail.com>
2025-11-12 21:52:56 +0100

    leker lite

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -10,9 +10,18 @@ public class MainView extends JFrame {
     private int windowWidth;
     private int windowHeight;
 
+    // "Akvarium"
     JLayeredPane container = new JLayeredPane();
-
+    // "Dekoration"?
     JPanel panel = new JPanel();
+
+    //Temporär bakgrund
+    //JLabel bakgrund = new JLabel(new ImageIcon("./src/view/images/akvarium1.jpg"));
+    JLabel bakgrund = new JLabel();
+
+    ImageIcon OGimage = new ImageIcon("./src/view/images/akvarium1.jpg");
+    Image scaledImage = OGimage.getImage().getScaledInstance(960, 540, Image.SCALE_SMOOTH);
+
 
 
     public MainView(int windowWidth, int windowHeight) {
@@ -26,17 +35,27 @@ public class MainView extends JFrame {
     private void initComponents() {
         this.setTitle(WINDOW_TITLE);
         setSize(new Dimension(windowWidth, windowHeight));
+        // Lägger Fönstret i mitten av skärmen
+        this.setLocationRelativeTo(null);
 
-        container.setBackground(Color.BLUE);
+        // LayeredPanes characteristics
+        container.setBounds(160,90,960, 540);
         container.setOpaque(true);
 
-        container.setBounds(160,90,960, 540);
+        // lägg till Akvarium till Fönstret
         this.add(container);
 
+        // Lägger till bilden till bakgrund(JLabel)
+        bakgrund.setIcon(new ImageIcon(scaledImage));
+        bakgrund.setBounds(0,0,960,540);
+        container.add(bakgrund, Integer.valueOf(0));
 
-        panel.setSize(new Dimension(200,200));
+        // Dekorationens characteristics
+        //panel.setSize(new Dimension(200,200));
+        panel.setBounds(0,330,200, 200);
         panel.setBackground(Color.green);
-        container.add(panel);
+        // Lägger till Dekorationen till Akvariet
+        container.add(panel, Integer.valueOf(1));
 
         // Temporärt
         // Load original image
@@ -54,9 +73,10 @@ public class MainView extends JFrame {
         ImageIcon scaledIcon = new ImageIcon(scaledImage);
 
         JLabel fisk = new JLabel(scaledIcon);
-        fisk.setBounds(100, 100, 50, 50);
+        fisk.setBounds(100, 400, 50, 50);
         fisk.setOpaque(false);
-        container.add(fisk, Integer.valueOf(1));
+        // Lägger till fisken till akvariet
+        container.add(fisk, Integer.valueOf(2));
 
         //this.pack();
         this.setLayout(null);
@@ -66,7 +86,10 @@ public class MainView extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         this.setResizable(false);
+
     }
+
+
 
 
 
