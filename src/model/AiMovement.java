7c103151753eec981ec362286aa54a5d6379dda8
Ole-JPG <olekipp@gmail.com>
2025-11-13 17:14:53 +0100

    innate direction + fixes

--- src/model/AiMovement.java
+++ src/model/AiMovement.java
@@ -3,21 +3,23 @@ package model;
 public class AiMovement implements IAi {
 
     private final IAquarium aquarium;
+    private double direction;
 
-    public AiMovement(IAquarium aquarium){
+    public AiMovement(IAquarium aquarium, double direction){
         this.aquarium = aquarium;
+        this.direction = direction;
     }
 
     @Override
     public void move(IFish fish){
-        Boolean isValid = (fish.getPos()+1).isValidPosition();
+        Boolean isValid = aquarium.isValidPosition(fish.getPos(), fish.getSize());
         if(!isValid){
-            double newDirection = Math.mod(fish.getDirection() + Math.PI) % (2*Math.PI);
+            double newDirection = (this.direction + Math.PI) % (2*Math.PI);
             // mirrors direction
-            fish.setDirection(newDirection);
+            this.direction = newDirection;
         }
-        int newX = (int) Math.ceil(fish.getPos().get(0) + (fish.getSpeed()*Math.cos(fish.getDirection()))); 
-        int newY = (int) Math.ceil(fish.getPos().get(1) + (fish.getSpeed()*Math.sin(fish.getDirection()))); 
+        int newX = (int) Math.ceil(fish.getPos().get(0) + (fish.getSpeed()*Math.cos(this.direction))); 
+        int newY = (int) Math.ceil(fish.getPos().get(1) + (fish.getSpeed()*Math.sin(this.direction))); 
         // newPos = oldPos + Speed*direction
         // M.ceil förhindrar ingen rörelse alls
         fish.setPos(newX,newY,0);
