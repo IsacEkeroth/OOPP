ba80578a3ef82af4def27238aaeb308f9370337f
IsacEkeroth <43499834+IsacEkeroth@users.noreply.github.com>
2025-11-17 11:44:28 +0100

    WIP view and controller (#23)
    * added: images, app-class, view-class
    
    * småfix
    
    * småfix2
    
    * småfix3
    
    * remove comment
    
    * added: layeredpane
    
    * WIP - smurfFish => Layeredpanes
    
    * leker lite
    
    ---------
    
    Co-authored-by: JoosefL <joseflot@gmail.com>

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import view.MainView;
+
+public class App {
+
+    private static final int windowWidth = 1280;
+    private static final int windowHeight = 720;
+
+
+
+
+
+
+
+    public static void start() {
+        MainView view = new MainView(windowWidth,windowHeight);
+    }
+
+
+
+}
