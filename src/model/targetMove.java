fc686a982bc9db665aed6860f9dda6ed470aee95
Ole-JPG <olekipp@gmail.com>
2025-11-17 11:26:10 +0100

    simpleMove (#20)
    * first stage ai
    
    * innate direction + fixes
    
    * fixed move and added empty targetMove
    
    * rounding for backwards-movement now works + empty targetMove
    
    * changes?
    
    * always true

---
+++ src/model/targetMove.java
@@ -0,0 +1,18 @@
+package model;
+
+public class targetMove implements IAi {
+
+    private final IAquarium aquarium;
+    private double direction;
+
+    public targetMove(IAquarium aquarium, double direction){
+        this.aquarium = aquarium;
+        this.direction = direction;
+    }
+
+    @Override
+    public void move(IFish fish){
+    // goal is to find shortest path to coordinates
+    // check pos() at every call
+    }
+}
