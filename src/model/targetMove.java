e6561a3475706095815f9ac80cf8b456f9ebfb15
Ole-JPG <olekipp@gmail.com>
2025-11-21 13:48:19 +0100

    pulling

--- src/model/targetMove.java
+++ src/model/targetMove.java
@@ -4,13 +4,23 @@ public class targetMove implements IAi {
 
     private final IAquarium aquarium;
     private double direction;
+    private Vec3 target;
 
     public targetMove(IAquarium aquarium, double direction){
         this.aquarium = aquarium;
         this.direction = direction;
+        this.target = null;
     }
     
     @Override
     public void move(IFish fish){
+    }
+
+    public void setTarget(Vec3 target){
+        this.target = target;
+    }
+
+    public Vec3 getTarget(){
+        return target;
     }
 }
