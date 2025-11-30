874f6f05e930312400cbd0ae10d28d95c20ccf6a
edinAh2002 <lufroloc.purp@outlook.com>
2025-11-30 16:22:03 +0100

    iEdible implemented,  issue #43

---
+++ src/main/java/com/grupp26/aquasim/model/Food.java
@@ -0,0 +1,47 @@
+package com.grupp26.aquasim.model;
+
+public class Food implements IEdible {
+
+    private Vec3<Integer> pos;
+    private int amount;
+    private boolean isEaten;
+
+    public Food(Vec3<Integer> pos, int amount) {
+        this.pos = pos;
+        this.amount = amount;
+        this.isEaten = false;
+    }
+
+    @Override
+    public void setPos(Vec3<Integer> pos) { this.pos = pos; }
+
+    @Override
+    public Vec3<Integer> getPos() { return pos; }
+
+    @Override
+    public void setAmount(int amount) {
+        this.amount = Math.max(0, amount);
+        this.isEaten = (this.amount == 0);
+    }
+
+    @Override
+    public int getAmount() { return amount; }
+
+    @Override
+    public void eat(int amount) {
+
+        if (isEaten || amount <= 0) return;
+
+        this.amount -= amount;
+
+        if (this.amount <= 0) {
+            this.amount = 0;
+            this.isEaten = true;
+        }
+
+    }
+
+    @Override
+    public boolean isEaten() { return isEaten; }
+
+}
