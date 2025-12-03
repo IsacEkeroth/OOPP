c93d279a58bcc9ab283134bfe551cfcdb66aa943
JoosefL <joseflot@gmail.com>
2025-12-03 20:41:08 +0100

    added: addFish-, feedFish-buttons. Only visual

--- src/main/java/com/grupp26/aquasim/view/MainView.java
+++ src/main/java/com/grupp26/aquasim/view/MainView.java
@@ -4,6 +4,7 @@ import com.grupp26.aquasim.model.IEntity;
 import com.grupp26.aquasim.model.ModelFacade;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,8 +16,11 @@ public class MainView extends JFrame implements IObserver {
     private DrawPanel drawPanel;
     private ModelFacade facade;
 
-    // bara testar att koppla en lyssnare till denna knapp
+    // controlPanel för framtida knappar
+    private final JPanel controlPanel = new JPanel();
     private final JButton addFishButton = new JButton("Add fish");
+    private final JButton feedFishButton = new JButton("Feed fish");
+
 
     public MainView(int windowWidth, int windowHeight) {
         this.windowWidth = windowWidth;
@@ -32,7 +36,20 @@ public class MainView extends JFrame implements IObserver {
 
         drawPanel = new DrawPanel(windowWidth, windowHeight);
         drawPanel.setOpaque(true);
+        // Denna behövdes lägga till
+        drawPanel.setLayout(null);
 
+        controlPanel.setLayout(new GridLayout(1,2));
+        controlPanel.add(addFishButton,0);
+        controlPanel.add(feedFishButton,1);
+        controlPanel.setBackground(Color.BLACK);
+        // Placering av controlPanel i fönstret
+        int buttonWidth = 150;
+        int buttonHeight = 50;
+        controlPanel.setBounds(10,windowHeight-90,buttonWidth,buttonHeight);
+
+        // Lägger controlPanel PÅ drawPanel
+        drawPanel.add(controlPanel);
         this.add(drawPanel);
         drawPanel.repaint();
 
