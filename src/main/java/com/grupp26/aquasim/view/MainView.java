6ecfdbf04c8228b45354e04df21cc89beb9d50d5
JoosefL <joseflot@gmail.com>
2025-12-04 17:41:48 +0100

    added: removeFishButton

--- src/main/java/com/grupp26/aquasim/view/MainView.java
+++ src/main/java/com/grupp26/aquasim/view/MainView.java
@@ -20,7 +20,7 @@ public class MainView extends JFrame implements IObserver {
     private final JPanel controlPanel = new JPanel();
     private final JButton addFishButton = new JButton("Add fish");
     private final JButton feedFishButton = new JButton("Feed fish");
-
+    private final JButton removeFishButton = new JButton("Remove fish");
 
     public MainView(int windowWidth, int windowHeight) {
         this.windowWidth = windowWidth;
@@ -36,21 +36,25 @@ public class MainView extends JFrame implements IObserver {
 
         drawPanel = new DrawPanel(windowWidth, windowHeight);
         drawPanel.setOpaque(true);
-        // Denna behövdes lägga till
+
+        // Denna behövdes lägga till, så vi har ingen layoutmanager.
+        // Vi använder absolute positioning.
         drawPanel.setLayout(null);
 
-        controlPanel.setLayout(new GridLayout(1,2));
+        controlPanel.setLayout(new GridLayout(1,3));
         controlPanel.add(addFishButton,0);
         controlPanel.add(feedFishButton,1);
+        controlPanel.add(removeFishButton,2);
         controlPanel.setBackground(Color.BLACK);
-        // Placering av controlPanel i fönstret
-        int buttonWidth = 150;
+
+        int buttonWidth = 300;
         int buttonHeight = 50;
+        // Placering av controlPanel på (x, y) i drawPanel
         controlPanel.setBounds(10,windowHeight-90,buttonWidth,buttonHeight);
 
         // Lägger controlPanel PÅ drawPanel
-        drawPanel.add(controlPanel);
         this.add(drawPanel);
+        drawPanel.add(controlPanel);
         drawPanel.repaint();
 
         this.setVisible(true);
@@ -88,8 +92,15 @@ public class MainView extends JFrame implements IObserver {
 
 
 
-    public JButton getAddFish() {
+    public JButton getAddFishButton() {
         return this.addFishButton;
     }
 
+    public JButton getFeedFishButton() {
+        return this.feedFishButton;
+    }
+
+    public JButton getRemoveFishButton() {
+        return this.removeFishButton;
+    }
 }
