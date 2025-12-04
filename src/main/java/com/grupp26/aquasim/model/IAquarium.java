4c101e0df972a2f4cf6ab37c002f41a36a0c2e5a
thelastseal <thelastseal@outlook.com>
2025-12-04 16:21:26 +0100

    Added extra attributes with getters/setters

--- src/main/java/com/grupp26/aquasim/model/IAquarium.java
+++ src/main/java/com/grupp26/aquasim/model/IAquarium.java
@@ -12,10 +12,31 @@ public interface IAquarium {
     boolean isValidPosition(Vec2<Integer> pos, Vec2<Integer> size);
 
     Vec2<Integer> getAquariumSize();
+    
+    int getTemperature();
+    
+    void setTemperature(int temp);
+    
+    int getAlgaeLevel();
+    
+    void setAlgaeLevel(int level);
+    
+    int getO2Conc();
+    
+    void setO2Conc(int conc);
+    
+    int getSalinity();
+    
+    void setSalinity(int salinity);
+    
+    int getPHLevel();
+    
+    void setPHLevel(int level);
+    
+    AquariumState getState();
 
     void tick();
     
-    AquariumState getState();
     
     void addDecoration(IDecoration decoration);
 }
