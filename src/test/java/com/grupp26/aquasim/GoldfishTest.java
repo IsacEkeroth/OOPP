9c1dbcea36ac01ac9934b932f909629061caaf5d
edinAh2002 <lufroloc.purp@outlook.com>
2025-12-04 11:36:07 +0100

    goldfish test renamed

--- src/test/java/com/grupp26/aquasim/GoldfishTest.java
+++ src/test/java/com/grupp26/aquasim/GoldfishTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class FishTest {
+public class GoldfishTest {
 
     private IAquarium aquarium;
     private Goldfish fish;
