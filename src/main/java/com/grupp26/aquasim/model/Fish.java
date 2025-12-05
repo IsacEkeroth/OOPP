8990898610a2837eb8fb3db4bb2330a5cdc44117
IsacEkeroth <43499834+IsacEkeroth@users.noreply.github.com>
2025-12-05 12:45:48 +0100

    fix fish (#76)

--- src/main/java/com/grupp26/aquasim/model/Fish.java
+++ src/main/java/com/grupp26/aquasim/model/Fish.java
@@ -13,20 +13,19 @@ public class Fish implements IFish {
     private int speed;
 
     private Vec2<Integer> size = new Vec2<>(50, 50);
-    private Vec3<Integer> pos;
+    private Vec3<Integer> pos = new Vec3<Integer>(100, 100, 1);
     private IBehaviour behaviour;;
 
     public Fish(IAquarium aquarium) {
 
         this.isAlive = true;
         this.aquarium = aquarium;
-
         this.health = 100;
         this.hunger = 0;
         this.age = 0;
         this.baseSpeed = 5;
         this.speed = baseSpeed;
-        this.behaviour = new GoldFishBehaviour(this, 0, 30); //30 is a placeholder
+        this.behaviour = new GoldFishBehaviour(this, 0, 30); // 30 is a placeholder
 
     }
 
@@ -102,9 +101,9 @@ public class Fish implements IFish {
             this.isAlive = false;
         }
 
-        speed = Math.max(1, baseSpeed - (hunger / 20));
+        speed = Math.max(1, baseSpeed + (hunger / 20));
 
         this.behaviour.update();
-        
+
     }
 }
