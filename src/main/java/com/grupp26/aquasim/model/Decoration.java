70158676d097209fab250ed4a215d568fc08d3dc
edinAh2002 <lufroloc.purp@outlook.com>
2025-12-08 11:18:29 +0100

    decoration test early implementation (issue #34) (#69)
    * decoration test early implementation
    
    * Rename DecoTest.java to DecorationTest.java
    
    ---------
    
    Co-authored-by: IsacEkeroth <43499834+IsacEkeroth@users.noreply.github.com>

--- src/main/java/com/grupp26/aquasim/model/Decoration.java
+++ src/main/java/com/grupp26/aquasim/model/Decoration.java
@@ -2,9 +2,19 @@ package com.grupp26.aquasim.model;
 
 public class Decoration implements IDecoration {
 
+    private final IAquarium aquarium;
+
     private int size;
 
     private Vec3<Integer> pos;
+
+    public Decoration(IAquarium aquarium, Vec3<Integer> pos) {
+
+        this.aquarium = aquarium;
+
+        this.pos = pos;
+
+    }
 
     @Override
     public int getSize() {
