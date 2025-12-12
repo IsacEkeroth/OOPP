e5a3804861f2640e8812392b1a4a93c23c587682
Isac Ekeroth <isac.ekeroth@icloud.com>
2025-12-12 19:25:59 +0100

    update FoodFactory to use register method (Registry-based Factory)

--- src/main/java/com/grupp26/aquasim/model/FoodFactory.java
+++ src/main/java/com/grupp26/aquasim/model/FoodFactory.java
@@ -1,18 +1,34 @@
 package com.grupp26.aquasim.model;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class FoodFactory {
-    IAquarium aquarium;
+    private final IAquarium aquarium;
+    private final Map<String, Supplier<IEdible>> registry = new HashMap<>();
 
     public FoodFactory(IAquarium aquarium) {
         this.aquarium = aquarium;
-    }
-
-    public IEdible createFood(String type) {
-
         int centerX = aquarium.getAquariumSize().getX() / 2;
         int centerY = 50;
         int centerZ = 1;
-        return new Food(new Vec3<Integer>(centerX, centerY, centerZ), 50, aquarium);
+
+        // Register register food types
+        register("base", () -> new Food(new Vec3<>(centerX, centerY, centerZ), 50, aquarium));
+    }
+
+    public void register(String type, Supplier<IEdible> FoodConstructor) {
+        registry.put(type, FoodConstructor);
+    }
+
+    public IEdible createFood(String type) {
+        Supplier<IEdible> FoodConstructor = registry.get(type);
+
+        if (FoodConstructor == null) {
+            throw new IllegalArgumentException("Unknown food type: " + type);
+        }
+        return FoodConstructor.get();
     }
 
 }
