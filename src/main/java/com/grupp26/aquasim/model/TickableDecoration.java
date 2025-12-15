61d2f02ba6236fda5770cdd371a6b471abb37af6
Isac Ekeroth <isac.ekeroth@icloud.com>
2025-12-15 12:03:59 +0100

    cahnge tickable to extend

--- src/main/java/com/grupp26/aquasim/model/TickableDecoration.java
+++ src/main/java/com/grupp26/aquasim/model/TickableDecoration.java
@@ -1,33 +1,12 @@
 package com.grupp26.aquasim.model;
 
-public class TickableDecoration implements IDecoration, ITickable {
+public class TickableDecoration extends Decoration implements ITickable {
 
     private final IAquarium aquarium;
 
-    private Vec2<Integer> size = new Vec2<Integer>(100, 100);
-
-    private Vec3<Integer> pos;
-
     public TickableDecoration(IAquarium aquarium, Vec3<Integer> pos) {
-
+        super(aquarium, pos);
         this.aquarium = aquarium;
-
-        this.pos = pos;
-
-    }
-
-    public Vec2<Integer> getSize() {
-        return new Vec2<Integer>(size.getX(), size.getY());
-    }
-
-    public void setPos(int x, int y, int z) {
-        pos.setX(x);
-        pos.setY(y);
-        pos.setZ(z);
-    }
-
-    public Vec3<Integer> getPos() {
-        return pos;
     }
 
     public void tick() {
