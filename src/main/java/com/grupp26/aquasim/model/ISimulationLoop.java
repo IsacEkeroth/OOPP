5befcefaafd80c23972e8ed349813818aece7a51
thelastseal <thelastseal@outlook.com>
2025-12-19 11:05:26 +0100

    Moved timer to Model (#122)
    * Moved timer to Model
    
    * made delay a variable, added simple javadoc

---
+++ src/main/java/com/grupp26/aquasim/model/ISimulationLoop.java
@@ -0,0 +1,11 @@
+package com.grupp26.aquasim.model;
+
+/**
+ * Interface representing the simulation loop of the aquarium simulation.
+ * It provides methods to start and stop the simulation.
+ */
+public interface ISimulationLoop {
+    void start();
+    
+    void stop();
+}
---
+++ src/main/java/com/grupp26/aquasim/model/SimulationLoop.java
@@ -0,0 +1,26 @@
+package com.grupp26.aquasim.model;
+
+import javax.swing.Timer;
+
+/**
+ * Simulation loop using a Swing Timer to periodically execute a given action.
+ */
+public class SimulationLoop implements ISimulationLoop {
+    private final Timer timer;
+    
+    public SimulationLoop(int delay, Runnable timerAction) {
+        this.timer = new Timer(delay, e -> timerAction.run());
+        
+        timer.start();
+    }
+
+    @Override
+    public void start() {
+        timer.start();
+    }
+    
+    @Override
+    public void stop() { // Maybe we want a pause method later?
+        timer.stop();
+    }
+}
