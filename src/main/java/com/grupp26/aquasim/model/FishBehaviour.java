cc5d48af234e080757efa89d2b49139363f490a9
thelastseal <thelastseal@outlook.com>
2025-12-19 14:37:25 +0100

    properly merged

--- src/main/java/com/grupp26/aquasim/model/FishBehaviour.java
+++ src/main/java/com/grupp26/aquasim/model/FishBehaviour.java
@@ -7,26 +7,14 @@ public class FishBehaviour implements IFishBehaviour {
     private IFishState passiveState;
     private IFishState hungerState;
     private IFishState deathState;
-<<<<<<< HEAD
     private IFishState loveSeekingState;
     private IFishState matingState;
-=======
     private IMovement passiveMovement; // varies based on fish type
->>>>>>> origin/main
     private int hungryAt;
 
     public FishBehaviour(IFish fish, double initialDirection, int hungryAt) {
         this.hungryAt = hungryAt;
         this.aquarium = fish.getAquarium();
-<<<<<<< HEAD
-
-        this.passiveState = new FishPassiveState(this, fish, new SimpleMove(this.aquarium, initialDirection), aquarium);
-        this.hungerState = new FishHungerState(this, fish, new TargetMove(this.aquarium, initialDirection), aquarium);
-        this.loveSeekingState = new FishLoveSeekingState(this, fish, new TargetMove(this.aquarium, initialDirection), aquarium);
-        this.matingState = new FishMatingState(this, fish, aquarium);
-
-        this.deathState = new FishDeathState(this, fish, aquarium);
-=======
         
         if (fish.getType().equals("Goldfish")) {
             passiveMovement = new SchoolMove(aquarium, initialDirection);
@@ -34,10 +22,11 @@ public class FishBehaviour implements IFishBehaviour {
         } else {
             passiveMovement = new SimpleMove(aquarium, initialDirection);
         }
-        this.passiveState = new PassiveState(this, fish, passiveMovement, aquarium);
-        this.hungerState = new HungerState(this, fish, new TargetMove(this.aquarium, initialDirection), aquarium);
-        this.deathState = new DeathState(this, fish, aquarium);
->>>>>>> origin/main
+        this.passiveState = new FishPassiveState(this, fish, passiveMovement, aquarium);
+        this.hungerState = new FishHungerState(this, fish, new TargetMove(this.aquarium, initialDirection), aquarium);
+        this.loveSeekingState = new FishLoveSeekingState(this, fish, new TargetMove(this.aquarium, initialDirection), aquarium);
+        this.matingState = new FishMatingState(this, fish, aquarium);
+        this.deathState = new FishDeathState(this, fish, aquarium);
         this.state = this.passiveState;
     }
 
